70a33e7f8cde64c1454c068cc3779c23e104b1d2
ramon <ramongomes95@outlook.com>
2024-04-05 19:58:32 -0300

    refactoring and keys function

---
+++ modal.js
@@ -0,0 +1,40 @@
+import { calcIMC } from "./script.js"
+export const Modal = {
+    divmodal:document.querySelector("#modal"),
+    h2: document.querySelector("#modal h2"),
+    span: document.querySelector("#modal span"),
+
+
+    open(){
+        this.divmodal.classList.remove("close")
+        this.divmodal.classList.add("open")
+    },
+
+    close(){
+        this.divmodal.classList.remove("open")
+        this.divmodal.classList.add("close")
+    },
+
+
+    classification(){
+        
+        if(calcIMC < 18.5){
+            this.span.innerHTML = `Você está abaixo do peso, procure um médico ou nutricionista.`
+        } else if(calcIMC >= 18.5 && calcIMC < 25) {
+            this.span.innerHTML = `Você está no peso ideal.`
+        } else if(calcIMC >= 25 && calcIMC <= 30) {
+            this.span.innerHTML = `Você está Obeso, reduza a ingestão de calorias.`
+        } else if(calcIMC > 30) {
+            this.span.innerHTML = `Você está com obesidade mórbida, procure um médico imediatamente.`
+        }
+    }
+
+}
+
+window.addEventListener("keydown", handleKeydown)
+function handleKeydown(event){
+    if( event.key ==="Escape"){
+        Modal.close()
+    }
+}
+    
--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+import { Modal } from "./modal.js"
+/****Variaveis****/
 let weight = document.querySelector("#weight");
 let height = document.querySelector("#height");
 let buttonCalculate = document.querySelector("#calculate")
-let buttonClose = document.querySelector("#btnclose")
-let div = ""
-let calcIMC = ""
+let buttonClose = document.querySelector("#btnClose")
+export let calcIMC = ""
 
 
+/*****Ouvidor de eventos*****/
 buttonCalculate.addEventListener("click",calculator)
-buttonClose.addEventListener("click",modalClose)
+buttonClose.addEventListener("click",btnClose)
+
+
+function btnClose(){
+    Modal.close()
+}
+
 
 function calculator(event){
     event.preventDefault()
@@ -15,66 +23,47 @@ function calculator(event){
     weight = document.querySelector("#weight")
     height = document.querySelector("#height")
     calcIMC = ((weight.value / (height.value**2))*10000).toFixed(1)
-    div = document.querySelector("#modal")
-    div.querySelector("h2").innerHTML = `Seu IMC é de ${calcIMC}`
-    classification()
-    errorBar()
-    toogle()
+    Modal.h2.innerHTML = `Seu IMC é de ${calcIMC}`
+    checker()
 }
 
 
-function classification(){
-    div = document.querySelector("#modal")
-    if(calcIMC < 18.5){
-        div.querySelector("span").innerHTML = `Você está abaixo do peso, procure um médico ou nutricionista.`
-    } else if(calcIMC >= 18.5 && calcIMC < 25) {
-        div.querySelector("span").innerHTML = `Você está no peso ideal.`
-    } else if(calcIMC >= 25 && calcIMC <= 30) {
-        div.querySelector("span").innerHTML = `Você está Obeso, reduza a ingestão de calorias.`
-    } else if(calcIMC > 30) {
-        div.querySelector("span").innerHTML = `Você está com obesidade mórbida, procure um médico imediatamente.`
-    }
-}
-
-
-function modalClose(){
-    toogle()
-    reset()
-}
-
-
-function toogle() {
-    if (div.classList.contains("close")) {
-        div.classList.remove("close")
-        div.classList.add("open")
-    } else {
-        div.classList.remove("open")
-        div.classList.add("close")
-    }
-}
-
-
-function errorBar() {
+function checker() {
     if (weight.value.includes(",") || weight.value.includes(".")) {
-        console.log("Peso contém vírgula ou ponto.")
-        div = document.querySelector("#alertError")
-    }
+        errorBar()
 
-    if (height.value.includes(",") || height.value.includes(".")) {
-        console.log("Altura contém vírgula ou ponto.")
-        div = document.querySelector("#alertError")
+    }else if (height.value.includes(",") || height.value.includes(".")) {
+        errorBar()
+
+    }else{
+        Modal.open()
+        Modal.classification()
+        reset()
     }
+}
+
+
+function errorBar(){
+    alertError.classList.remove("close")
+    alertError.classList.add("open")
 }
 
 
 function reset(){
     weight.value = ""
     height.value = ""
-    div = document.querySelector("#alertError")
-    if (div.classList.contains("open")) {
-        div.classList.remove("open")
-        div.classList.add("close")
+    let alertError= document.querySelector("#alertError")
+    if (alertError.classList.contains("open")) {
+        alertError.classList.remove("open")
+        alertError.classList.add("close")
     }
 }
 
+
+window.addEventListener("keydown", handleKeydown)
+function handleKeydown(event){
+    if( event.key ==="Enter"){
+        checker()
+    }
+}
 
